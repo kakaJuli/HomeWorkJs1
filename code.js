cb63369fe0a0d0f9bb3332061f446d1b46f6d5eb
mullametova <jkovalenok@mail.ru>
2022-04-08 00:24:22 +0300

    first commit

---
+++ code.js
@@ -0,0 +1,134 @@
+console.log ("  Бывало, спит у ног собака,");
+console.log ("  костер занявшийся гудит,");
+console.log ("  и женщина из полумрака");
+console.log ("  глазами зыбкими глядит.");
+
+console.log (   "Потом под пихтою приляжит");
+console.log (   "на куртку рыжую мою");
+console.log (   "и мне, задумчивая, скажет:");
+
+console.log (   "А ну-ка, спой!..-и я спою.");
+
+// задание 10
+
+
+let text = "В ту же минуту, безо всяких туч,"
+    textone = "блеснула молния и ударил гром,"
+    texttwo = "индо земля зашаталась под ногами-и вырос,"
+    textthree = "как будто из-под земли, перед купцом:"
+    textfoer = "зверь не зверь, человек не человек,"
+    textfive = "фа так какое то чудище, страшное и мохнатое,"
+    textsix = "и заревел он голосом диким..."
+console.log (text, textone, texttwo, textthree, textfoer, textfive, textsix);
+
+
+// Задание 2
+
+
+let a1 = (5 % 3);
+console.log(a1);
+let a2 = (3 % 5);
+console.log(a2);
+let a3 = (5 + '3');
+console.log(a3);
+let a4 = ('5' + 3);
+console.log(a4);
+let a5 = (75 + 'кг');
+console.log(a5);
+let a6 = (785 * 653);
+console.log(a6);
+let a7 = (100 / 25);
+console.log(a7);
+let a8 = (0 * 0);
+console.log(a8);
+let a9 = (0 / 2);
+console.log(a9);
+let a10 = (89 / 0);
+console.log(a10);
+let a11 = (98 + 2);
+console.log(a11);
+let a12 = (5 - 98);
+console.log(a12);
+let a13 = (8 + 56 * 4) / (5 + 2);
+console.log(a13);
+let a14 = (9 - 12) * 7 / (5 + 2);
+console.log(a14);
+let a15 = +"123";
+console.log(a15);
+let a16 = 1 || 0;
+console.log(a16);
+let a17 = false || true;
+console.log(a17);
+let a18 = true > 0;
+console.log(a18);
+
+
+// Задание 3
+
+
+let width = 10;
+let height = 23;
+let SPryam = width * height;
+console.log(SPryam);
+
+
+// Задание 4 
+
+
+let heightC = 10;
+let VCilinder = height * a7;
+console.log(VCilinder);
+
+
+// Задание 5 
+
+
+let radius = 5;
+let SKruga = 2 * 3.14 * 5;
+console.log(SKruga);
+
+
+//Заданиие 6
+
+
+/*let a = 5;
+let b = 7;
+let h = 10;
+let STrap = (parseInt(a) * parseInt(b) / 2) * parseInt(h);
+console.log(STrap);*/
+
+
+// Задание 7
+
+
+let s = 2000000;
+let p = 10;
+let years = 5;
+let pol = 5 * (1 + 5) ** 60;
+let pol2 = (1 + 5) ** 60 - 1;
+let Pereplata = pol / pol2;
+console.log(Pereplata);
+
+
+// Задание 8
+
+
+//let a = 8;
+//let b = 3;
+//let x = a + 2(x - b);
+
+
+
+// Задание 9 
+
+console.log(`
+    Бывало, спит у ног собака,
+    костер занявшийся гудит,
+    и женщина из полумрака
+    глазами зыбкими глядит.
+    
+    Потом под пихтою приляжет
+    на куртку рыжую мою
+    и мне, задумчивая, скажет:
+    
+    "А ну-ка, спой!.."- и я пою.`);
